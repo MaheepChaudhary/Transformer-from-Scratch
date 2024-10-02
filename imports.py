5ce0da5e27dee641bb1fb511aa97d4bd6e7ae0a8
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2024-10-02 14:19:15 +0530

    initial commit

---
+++ imports.py
@@ -0,0 +1,6 @@
+import numpy as np
+import math 
+import torch as t
+from torch import nn
+import typing
+from torch import Tensor
---
+++ main.py
@@ -0,0 +1,5 @@
+from imports import *
+
+# Taking a simple sent that will be passed into the transformer. 
+sent: Tensor = t.randn((4))
+
