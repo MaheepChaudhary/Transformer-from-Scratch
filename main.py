e44211e79bc8594b152b787c22aff2f8238f4c76
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2024-10-13 12:25:21 +0530

    debugging

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 import argparse
 
 from imports import *
-from encoder import encoder
-from decoder import decoder
+from encoder import Encoder as encoder
+from decoder import Decoder as decoder
 
 # Taking a simple sent that will be passed into the transformer. 
 sent: t.Tensor = t.randint(0, 8, (4,))
@@ -13,26 +13,25 @@ out_sent: t.Tensor = t.randint(0, 8, (4,))
 print(f"Input sent: {sent}")
 print(f"Output sent: {out_sent}")
 
-def output(decoder_output: t.Tensor) -> t.Tensor:
-    linear_output = nn.Linear(512, 8)(decoder_output)  # 8 is the vocabulary size
-    return linear_output
+class Transformer(nn.Module):
+    def __init__(self, num_heads: int, sent: t.Tensor, out_sent: t.Tensor):
+        super(Transformer, self).__init__()
+        self.encoder = encoder(num_heads, sent)
+        self.decoder = decoder(num_heads, out_sent)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument("--num_heads", type=int, default=2, help="Number of heads for multi-head attention.")
-    
-    args = parser.parse_args()
+    def output(self, decoder_output: t.Tensor) -> t.Tensor:
+        linear_output = nn.Linear(512, 8)(decoder_output)  # 8 is the vocabulary size
+        return linear_output
 
-    ce_loss = nn.CrossEntropyLoss()
-    
-    # Creating the encoder object
-    enc_output = encoder(args.num_heads, sent).forward()
-    decoder_output = decoder(args.num_heads, out_sent, enc_output).forward()
-    predicted_logits = output(decoder_output)
-    print(predicted_logits)
+    def forward(self) -> t.Tensor:
+        encoder_output = self.encoder.forward()
+        decoder_output = self.decoder.forward(encoder_output) 
+        return self.output(decoder_output)
+# parser = argparse.ArgumentParser(
+ce_loss = nn.CrossEntropyLoss()
+num_heads = 2
 
-    # To get the contribution of each word to the loss, we can use nn.CrossEntropyLoss with reduction='none'
-    ce_loss_none = nn.CrossEntropyLoss(reduction='none')
-    loss_contributions = ce_loss_none(predicted_logits.view(4, 8), out_sent)
-    print(f"Contribution of each word to the loss: {loss_contributions}")
+predicted_logits = Transformer(num_heads, sent, out_sent).forward()
+ce_loss_none = nn.CrossEntropyLoss(reduction='none')
+loss_contributions = ce_loss_none(predicted_logits.view(4, 8), out_sent)
+print(f"Contribution of each word to the loss: {loss_contributions}")
